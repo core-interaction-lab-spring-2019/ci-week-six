c11fcd021be34aedeeec783d47aca915cb4734ce
Dani Delossantos <delod999@newschool.edu>
2019-03-13 19:09:22 -0400

    fixed modal

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $thumbnail = $('.gallery__item');
 
     $modal = $('.modal');
-    $modal_image = $('.modal__image');
+    $modal_image = $('.modal--image');
 
     $thumbnail.click(function() {
         img_src = $(this).attr('data-href');
