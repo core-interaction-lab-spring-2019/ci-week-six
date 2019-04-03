1eff167167cfa9d610acb7eb84264efb1e7a5203
Dani Delossantos <delod999@newschool.edu>
2019-04-03 10:44:36 -0400

    changing styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,78 @@
 $(document).ready(function () {
   
     $thumbnail = $('.gallery__item');
-
     $modal = $('.modal');
     $modal_image = $('.modal--image');
+    $modal_close = $('.modal-close');
+    $next_button = $('.next-button');
+    $prev_button = $('.prev-button');
 
     $thumbnail.click(function() {
+        $current_image = $(this);
         img_src = $(this).attr('data-href');
+
+        if($(this).next().length) {
+        imgnext = $(this).next().attr('data-href');
+        $next_button.attr('data-href',imgnext);
+        $next_button.removeClass('no-link');
+        } else {
+            $next_button.addClass('.no-link');
+        }
+
+        if($(this).prev().length) {
+            imgprev = $(this).prev().attr('date-href');
+            $prev_button.attr('date-href',imgprev);
+            $prev_button.removeClass('no-link');
+        }else{
+            $prev_button.addClass('.no-link');
+        }
+
         $modal_image.attr('src', img_src);
         $modal.addClass('modal--open');
         
-    })
+    });
+
+    $next_button.click(function(){
+        img_src = $(this).attr('data-href');
+        $current_image = $current_image.next();
+        img_next = $current_image.next().attr('data-href');
+        img_prev = $current_image.prev().attr('data-href');
+        
+        $modal_image.attr('src',img_src);
+        $next_button.attr('data-href', img_next);
+        $prev_button.attr('data-href', img_prev);
+        $prev_button.removeClass('no-link');
+
+        if (!$current_image.next().length) {
+        $next_button.addClass('no-link');
+        $next_button.attr('data-href', '');
+        }
+
+
+    });
+
+    $prev_button.click(function(){
+        img_src = $(this).attr('data-href');
+        $current_image = $current_image.prev();
+        img_next = $current_img.next().attr('data-href');
+        img_prev = $current_img.prev().attr('data-href');
+
+        $modal_image.attr('src',img_src);
+        $next_button.attr('data-href', img_next);
+        $prev_button.attr('data-href', img_prev);
+
+
+        if (!$current_image.next().length) {
+            $prev_button.addClass('no-link');
+            $prev_button.attr('data-href', '');
+            }
+    
+    });
+
+    $modal_close.click(function(){
+        $modal.removeClass('modal--open');
+    });
+
+    
 
 }); 
